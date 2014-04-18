654dfbcfb56750658592bd3b6bf57eeb7990de4b
Kuang-che Wu <kcwu@chromium.org>
2014-04-18 18:35:28 +0800

    short_lo, 98*476=46648

--- ai_modules/short_lo.py
+++ ai_modules/short_lo.py
@@ -1,4 +1,4 @@
-# 86*621=53406
+# 98*476=46648
 class AI:
  i=0
- def getNextMove(s,g):s.i+=1;return['left','right','up','down'][s.i%4]
+ def getNextMove(s,g):s.i+=1;return'left right up up up up up down'.split()[s.i%8]
